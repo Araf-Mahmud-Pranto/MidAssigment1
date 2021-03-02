9f17a99fd15ab9c2db66929709e617809a33d8a4
Araf-Mahmud-Pranto <78262425+Araf-Mahmud-Pranto@users.noreply.github.com>
2021-03-03 00:07:42 +0600

    Add files via upload

---
+++ Bank.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midassigment
+{ 
+   
+    
+        class Bank
+        {
+            private string bankName;
+            private Account[] myBank;
+
+            public Bank(string bankName, int size)
+            {
+                this.bankName = bankName;
+                this.myBank = new Account[size];
+            }
+
+            public string BankName
+            {
+                set { this.bankName = value; }
+                get { return this.bankName; }
+            }
+
+            public Account[] MyBank
+            {
+                get { return this.myBank; }
+            }
+        public void AddAccount()
+        {
+
+        }
+        public void DeleteAccount()
+        {
+
+        }
+        public void Transaction()
+        {
+
+        }
+        public int SearchAccount()
+        {
+
+        }
+
+
+
+}
